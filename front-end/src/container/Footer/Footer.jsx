2394a8984b2ae99850f4754c39f2bc6888d2c9b7
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2022-03-20 23:32:28 +0100

    delete form contact mail js

--- front-end/src/container/Footer/Footer.jsx
+++ front-end/src/container/Footer/Footer.jsx
@@ -1,37 +1,9 @@
-import React, { useState, useRef } from 'react';
-//import emailjs from '@emailjs/browser';
+import React, { useState } from 'react';
 
 import { AppWrap, MotionWrap } from '../../wrapper';
 import { client } from '../../client';
 import './Footer.scss';
 
-
-// const Footer = () => {
-//   const form = useRef();
-
-//   const sendEmail = (e) => {
-//     e.preventDefault();
-
-//     emailjs.sendForm('process.env.YOUR_SERVICE_ID', 'process.env.YOUR_TEMPLATE_ID', form.current, 'process.env.YOUR_USER_ID')
-//       .then((result) => {
-//           console.log(result.text);
-//       }, (error) => {
-//           console.log(error.text);
-//       });
-//   };
-
-//   return (
-//     <form ref={form} onSubmit={sendEmail}>
-//       <label>Name</label>
-//       <input type="text" name="user_name" />
-//       <label>Email</label>
-//       <input type="email" name="user_email" />
-//       <label>Message</label>
-//       <textarea name="message" />
-//       <input type="submit" value="Send" />
-//     </form>
-//   );
-// };
 
 const Footer = () => {
   const [formData, setFormData] = useState({ name: '', email: '', message: '' });
